38be58f0b07f206830eb9549873cd0f0499b99c6
Grace Nweke <lagra2001@gmail.com>
2023-08-10 15:29:45 -0700

    rectifying task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
 print("{0} + {1} = {2}".format(a, b, add(a, b)))
