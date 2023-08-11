ef5b0c3cd564adf3a460e6758d6459c740d2687e
Grace Nweke <lagra2001@gmail.com>
2023-08-10 18:21:59 -0700

    correcting task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arg_len = len(sys.argv)
-    i = 0
-    if arg_len >= 2:
-        if arg_len == 2:
-            print("{} argument:".format(arg_len - 1))
-        else:
-            print("{} arguments:".format(arg_len - 1))
-        for x in sys.argv:
-            if i == 0:
-                i = i + 1
-                continue
-            print(f"{i}: {x}")
-            i = i + 1
+    args = sys.argv[1:]
+    i = 1
+    if len(args) == 0:
+        print("0 arguments.")
+    elif len(args) == 1:
+        print("{} argument:".format(len(args)))
     else:
-        print("0 arguments:")
+        print("{} arguments:".format(len(args)))
+    for arg in args:
+        print("{}: {}".format(i, arg))
+        i = i + 1
