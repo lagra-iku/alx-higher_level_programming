9c62f011927de65ded3a13542e63a3827089ce68
Grace Nweke <lagra2001@gmail.com>
2023-08-14 12:58:22 -0700

    solving for 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= 1 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         new_list = my_list[:idx] + [element] + my_list[idx+1:]
         return new_list
     else:
