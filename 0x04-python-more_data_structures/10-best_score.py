01c9549dd414834f0e78376bdec2197126cf8b29
Grace Nweke <lagra2001@gmail.com>
2023-08-16 15:44:27 -0700

    solving for 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
-    max_key = None
-    max_value =
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_key = key
-            max_value = value
-    return max_key
+    else:
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
