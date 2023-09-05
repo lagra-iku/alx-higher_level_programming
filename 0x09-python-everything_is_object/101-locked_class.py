f113e5bf4b52b79802d492d92756d6a55baacc1a
Grace Nweke <lagra2001@gmail.com>
2023-09-05 14:35:28 -0700

    adding docstring to task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defines class LockedClass"""
+
+
 class LockedClass:
+    """
+    prevents user from creating new instance
+    """
     __slots__ = ['first_name']
 
     def __init__(self):
