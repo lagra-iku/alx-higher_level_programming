c90eb4e9e0ef3e0157137f1087150c9620aa7a15
Grace Nweke <lagra2001@gmail.com>
2023-09-12 17:37:52 -0700

    solving for 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Geometry Module
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry
+    """
+    def __init__(self):
+        pass
+    
+    def area(self):
+        """
+        func that raises an Exception
+        Raises:
+            area() is not implemented
+        """
+        raise Exception("area() is not implemented")
