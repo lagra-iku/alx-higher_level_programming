e249f0551a58d11dcd81893f6b18cf0e651deb64
Grace Nweke <lagra2001@gmail.com>
2023-09-12 17:10:44 -0700

    solving for 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Lookup
+"""
+
+
+def lookup(obj):
+    """
+    a function that returns the list of available
+    attributes and methods of an object
+    Returns: a list of above
+    """
+    return dir(obj)
