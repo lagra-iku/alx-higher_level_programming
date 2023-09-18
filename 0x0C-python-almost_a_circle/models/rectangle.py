424186c0db559aec75b094be71bc0a438fee43e2
Grace Nweke <lagra2001@gmail.com>
2023-09-18 14:33:00 -0700

    solving for task 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,3 +118,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Calculates the current rectangle area
+        Returns:
+            area of rectangle
+        """
+        return self.__width * self.__height
