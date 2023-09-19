b72a09ea105a326a255c363cf238b6d2f31b1f6f
Grace Nweke <lagra2001@gmail.com>
2023-09-18 17:48:15 -0700

    solving for task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,14 @@ class Base:
             return ("[]")
         else:
             return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file:"""
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                json_data = cls.to_json_string(list_objs)
+                file.write(json_data)
