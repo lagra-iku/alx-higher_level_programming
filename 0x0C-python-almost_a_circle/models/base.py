4f3c8238eee72235c94a50a1fb0e0055fef9a3ab
Grace Nweke <lagra2001@gmail.com>
2023-09-18 18:05:59 -0700

    solving for task 18-1119

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
                 file.write("[]")
             else:
                 json_serialize = [i.to_dictionary() for i in list_objs]
-                json_data = cls.to_json_string(json_serialize)
+                json_data = Base.to_json_string(json_serialize)
                 file.write(json_data)
 
     @staticmethod
@@ -49,3 +49,27 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances"""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_serialize = Base.from_json_string(file.read())
+                return [cls.create(**d) for d in json_serialize]
+        except IOError:
+            return []
