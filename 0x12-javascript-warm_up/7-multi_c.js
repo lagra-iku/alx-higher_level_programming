8d9c745169d5dff2225da020f8bc730027dfb0a4
Grace O Nweke <lagra2001@gmail.com>
2023-11-13 18:48:20 -0800

    solving for 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (parseInt(process.argv[2])) {
+  for (let x = 0; x < process.argv[2]; x++) console.log('C is fun');
+} else {
+  console.log('Missing number of occurrences');
+}
