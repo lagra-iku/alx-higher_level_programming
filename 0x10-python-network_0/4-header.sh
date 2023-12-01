6e8942ff419348034914d45c9619c4544dc317de
Grace Nweke <lagra2001@gmail.com>
2023-11-30 17:09:58 -0800

    correcting task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #a Bash script that takes in a URL as an argument, sends a GET request to the URL
 #A header variable X-School-User-Id must be sent with the value 98
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sHX GET "X-School-User-Id: 98" "$1"
