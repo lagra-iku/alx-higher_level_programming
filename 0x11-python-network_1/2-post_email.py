1bb2da877aac053bee63617776816dafa8184b56
Grace Nweke <lagra2001@gmail.com>
2023-12-01 15:10:10 -0800

    solving for task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a python script that takes arg and post and displays the response
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    email = {"email" : sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print(body)
