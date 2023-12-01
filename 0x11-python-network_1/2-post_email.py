b71b4c918bad162306080f98cb9adfe34fe66db4
Grace Nweke <lagra2001@gmail.com>
2023-12-01 15:21:00 -0800

    correcting task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import urllib.parse
 if __name__ == "__main__":
     email = {"email" : sys.argv[2]}
     data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
+    data = data.encode('utf-8')
     request = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(request) as response:
-        body = response.read()
+        body = response.read().decode('utf-8')
         print(body)
