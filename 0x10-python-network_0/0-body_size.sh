e6b17988fb9d05515f29461f00637f3e2d13cfef
Grace Nweke <lagra2001@gmail.com>
2023-11-30 16:49:12 -0800

    correcting task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #  a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -Is "$1" | grep Content-Length| cut -d: -f2 | tr -d ' '
+curl -s "$1" | wc -c
