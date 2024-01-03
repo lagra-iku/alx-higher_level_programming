eb57c9e02d225d9f44e8e9c1732557c714eb79a8
Grace O. Nweke <lagra2001@gmai.com>
2024-01-03 04:16:45 -0800

    solving task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* a script that display the status code of a GET request. */
+const request = require('request');
+
+const movieID = process.argv[2];
+const starwar = "https://swapi-api.alx-tools.com/api/films/" + movieID;
+
+request(starwar, function (error, status, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
